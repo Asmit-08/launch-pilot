fde6fadecafe309a729608ebb58db549a3bd293a
Asmit-08 <tiwariasmit53@gmail.com>
2026-07-27 09:55:49 +0530

    Add Google Analytics and remove node_modules

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PendoInitializer from "@/components/PendoInitializer";
 import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -70,9 +71,13 @@ export default function RootLayout({
   </Script>
       </head>
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        <PendoInitializer />
-        {children}
-      </body>
+  <PendoInitializer />
+  {children}
+
+  <GoogleAnalytics
+    gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!}
+  />
+</body>
     </html>
   );
 }
