0dbd52f1cf2f345d6eb5f52758d571da32531994
Asmit-08 <tiwariasmit53@gmail.com>
2026-08-24 08:52:33 +0530

    Pendo and Plausible Removed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import PendoInitializer from "@/components/PendoInitializer";
-import Script from "next/script";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
@@ -82,46 +80,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
     >
-      <head>
-        {/* Pendo */}
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-(function(apiKey){
-    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
-    v=['initialize','identify','updateOptions','pageLoad','track','trackAgent'];for(w=0,x=v.length;w<x;++w)(function(m){
-    o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-    y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
-    z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-})('5622a514-fc6f-4b3a-bc20-ccc6bd722123');
-`,
-          }}
-        />
-
-        {/* Plausible */}
-        <Script
-          src="https://plausible.io/js/pa-Z354mXFgyLfvrrilQKmbx.js"
-          strategy="afterInteractive"
-        />
-
-        <Script id="plausible-init" strategy="afterInteractive">
-          {`
-            window.plausible = window.plausible || function() {
-              (plausible.q = plausible.q || []).push(arguments);
-            };
-
-            plausible.init = plausible.init || function(i) {
-              plausible.o = i || {};
-            };
-
-            plausible.init();
-          `}
-        </Script>
-      </head>
-
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        <PendoInitializer />
-
         {children}
 
         <GoogleAnalytics
